49b7d8f55130d1fd60b1d629edee97aee231d883
jia-tang <54199432+jia-tang@users.noreply.github.com>
2021-07-31 14:02:44 +0800

    asdf

--- jia.py
+++ jia.py
@@ -106,7 +106,7 @@ def main():
                 write_to_exchange(exchange, buy_VALE)
                 write_to_exchange(exchange, sell_VALBZ)
 
-        if message["type"] == "fill" or message["symbol"] == "VALBZ" or message["symbol"] == "VALE" : 
+        if message["type"] == "fill": 
             print(message)
 
 
